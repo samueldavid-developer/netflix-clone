b6c007e01b844fbd4919b0cdfea7bb46fc5073e8
samueldavid-developer <samueldiazdeveloper@gmail.com>
2026-01-07 01:13:20 -0400

    Añadidos scripts de despliegue

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import './index.css'
-createRoot(document.getElementById('root')).render(
+
+// El signo ! al final soluciona el error que te marca TypeScript
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
     <App />
   </StrictMode>,
-  
 )
